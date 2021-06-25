4ba5b60c7170d5ea7c08562c39a149ee745371a5
flourencewangi <flourencewangi@gmail.com>
2021-06-25 18:58:02 +0300

    calculator

--- move.js
+++ move.js
@@ -3,21 +3,133 @@ function myfunction(a,b){
   return a*b;
 
 }
+  var a=7
 
-age=11
-function checkage(){if(age<18){
-  
-  console.log('minor');}
-  else{console.log('adult');}
+function check7(){if(a=7){
+  console.log('7');}
+  else{console.log('nil');}
 }
-checkage();
-  
-var sum=2
+check7();
+  var a=8
 
-function checksum(){if(sum<2){
+function check8(){if(a=8){
+  
+  console.log('8');}
+  else{console.log('nil');}
+}
+check8();
+  var a=9
+
+function check9(){if(a=9){
+  
+  console.log('9');}
+  else{console.log('nil');}
+}
+check9();
+  var a=4
+
+function check4(){if(a=4){
+  
+  console.log('4');}
+  else{console.log('nil');}
+}
+check4();
+  var a=5
+
+function check5(){if(a=5){
+  
+  console.log('5');}
+  else{console.log('nil');}
+}
+check5();
+  var a=6
+
+function check6(){if(a=6){
+  
+  console.log('6');}
+  else{console.log('nil');}
+}
+check6();
+  var a=1
+
+function check1(){if(a=1){
+  
+  console.log('1');}
+  else{console.log('nil');}
+}
+check1();
+  var a=2
+
+function check2(){if(a=2){
   
   console.log('2');}
   else{console.log('nil');}
 }
-checksum();
+check2();
+  var a=3
+
+function check3(){if(a=3){
   
+  console.log('3');}
+  else{console.log('nil');}
+}
+check3();
+  var a=0
+
+function check0(){if(a=0){
+  
+  console.log('0');}
+  else{console.log('nil');}
+}
+check0();
+  var a=1
+
+function checkd(){if(a=1){
+  
+  console.log('÷');}
+  else{console.log('nil');}
+}
+checkd();
+  var a=1
+
+function checkx(){if(x=x){
+  
+  console.log('*');}
+  else{console.log('nil');}
+}
+checkx();
+  var a=1
+
+function checkm(){if(a=1){
+  
+  console.log('-');}
+  else{console.log('nil');}
+}
+checkm();
+  var a=1
+
+function checke(){if(a=1){
+  
+  console.log('=');}
+  else{console.log('nil');}
+}
+checke();
+  var a=0
+
+function checkdot(){if(a=0){
+  
+  console.log('.');}
+  else{console.log('.');}
+}
+checkdot();
+  
+var a = 0
+
+function checksum() {
+  if (a = 2) {
+
+    console.log('+');
+  }
+  else { console.log('nil'); }
+}
+checksum();
