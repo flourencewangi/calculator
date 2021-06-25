05a3ee92f6316ad7bbeae08e716ec0f3dc142d5e
flourencewangi <flourencewangi@gmail.com>
2021-06-25 17:30:45 +0300

    calculator

--- move.js
+++ move.js
@@ -1,5 +1,13 @@
-function Add(a,b){
-  console.log(a+b);
-}
-Add(5+6);
+let x=myfunction(4,2)
+function myfunction(a,b){
+  return a*b;
 
+}
+
+
+function checkage(){if(age<18){
+  var age=16
+  console.log('minor');}
+  else{console.log('adult');}
+  checkage();
+}
